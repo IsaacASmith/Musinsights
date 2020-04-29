4a22003f17759c16c17508056788b7458b8eeb83
Isaac Smith <isaac.smith.sf@gmail.com>
2020-04-28 23:58:35 -0500

    added transition in for top level scores, cleaned out docs

---
+++ src/client-app/src/helpers/DrawingHelper.js
@@ -0,0 +1,35 @@
+class DrawingHelper {
+  drawBGCircle (canvasId) {
+    const canvas = document.getElementById(canvasId)
+    const ctx = canvas.getContext('2d')
+    ctx.strokeStyle = '#000'
+    ctx.lineWidth = 50
+    ctx.beginPath()
+    ctx.arc(400, 400, 350, 0, 2 * Math.PI)
+    ctx.stroke()
+  }
+
+  drawValueCircle (canvasId, percentToDraw) {
+    const canvas = document.getElementById(canvasId)
+    const ctx = canvas.getContext('2d')
+    ctx.translate(400, 400)
+    ctx.rotate(270 * Math.PI / 180)
+    ctx.translate(-400, -400)
+
+    var i = 0
+    var timer = setInterval(function () {
+      ctx.strokeStyle = '#344a5f'
+      ctx.lineWidth = 50
+      ctx.beginPath()
+      ctx.arc(400, 400, 350, 0, 2 * Math.PI * percentToDraw * i / 100, false)
+      ctx.stroke()
+      i = i + 1
+      console.log('drawing at ', i)
+      if (i >= 100) {
+        clearInterval(timer)
+      }
+    }, 10)
+  }
+}
+
+export default DrawingHelper
